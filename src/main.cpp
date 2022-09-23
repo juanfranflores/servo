92703d749ed0d842aa543ecc8b72a4e44c325035
Juan Flores <juan27fp@gmail.com>
2022-09-23 19:38:17 -0300

    pruebo servo MG946R

---
+++ src/main.cpp
@@ -0,0 +1,68 @@
+/*
+ * ESP32 Servo Example Using Arduino ESP32 Servo Library
+ * John K. Bennett
+ * March, 2017
+ * 
+ * This sketch uses the Arduino ESP32 Servo Library to sweep 4 servos in sequence.
+ * 
+ * Different servos require different pulse widths to vary servo angle, but the range is 
+ * an approximately 500-2500 microsecond pulse every 20ms (50Hz). In general, hobbyist servos
+ * sweep 180 degrees, so the lowest number in the published range for a particular servo
+ * represents an angle of 0 degrees, the middle of the range represents 90 degrees, and the top
+ * of the range represents 180 degrees. So for example, if the range is 1000us to 2000us,
+ * 1000us would equal an angle of 0, 1500us would equal 90 degrees, and 2000us would equal 1800
+ * degrees.
+ * 
+ * Circuit:
+ * Servo motors have three wires: power, ground, and signal. The power wire is typically red,
+ * the ground wire is typically black or brown, and the signal wire is typically yellow,
+ * orange or white. Since the ESP32 can supply limited current at only 3.3V, and servos draw
+ * considerable power, we will connect servo power to the VBat pin of the ESP32 (located
+ * near the USB connector). THIS IS ONLY APPROPRIATE FOR SMALL SERVOS. 
+ * 
+ * We could also connect servo power to a separate external
+ * power source (as long as we connect all of the grounds (ESP32, servo, and external power).
+ * In this example, we just connect ESP32 ground to servo ground. The servo signal pins
+ * connect to any available GPIO pins on the ESP32 (in this example, we use pins
+ * 22, 19, 23, & 18).
+ * 
+ * In this example, we assume four Tower Pro SG90 small servos.
+ * The published min and max for this servo are 500 and 2400, respectively.
+ * These values actually drive the servos a little past 0 and 180, so
+ * if you are particular, adjust the min and max values to match your needs.
+ * Experimentally, 550 and 2350 are pretty close to 0 and 180.
+ */
+
+#include <ESP32Servo.h>
+
+// create four servo objects 
+Servo servo1;
+// Published values for SG90 servos; adjust if needed
+int minUs = 500;
+int maxUs = 2400;
+
+// These are all GPIO pins on the ESP32
+// Recommended pins include 2,4,12-19,21-23,25-27,32-33
+// for the ESP32-S2 the GPIO pins are 1-21,26,33-42
+int servo1Pin = 15;
+
+int pos = 0;      // position in degrees
+ESP32PWM pwm;
+void setup() {
+	Serial.begin(115200);
+	servo1.setPeriodHertz(50);
+
+}
+
+void loop() {
+	servo1.attach(servo1Pin, minUs, maxUs);
+  
+  servo1.write(0);
+  delay(2000);
+
+  servo1.write(92);
+  delay(2000);
+
+  servo1.write(184);
+  delay(2000);
+}
